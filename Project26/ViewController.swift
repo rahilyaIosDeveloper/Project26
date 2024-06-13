83bd6615b331d0cf51f14fe8dbacb9e39ac6282b
rahilyanz <rahaomurzakova@gmail.com>
2024-06-13 19:17:44 +0600

    Initial Commit

---
+++ Project26/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project26
+//
+//  Created by Rahilya Nazaralieva on 13/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
